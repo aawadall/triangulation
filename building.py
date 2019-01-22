40fd35fbdf68e54f782974e954f56e7719a81f5e
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-22 12:44:22 -0500

    building depicted properly

--- building.py
+++ building.py
@@ -11,12 +11,112 @@ class Building(object):
         x = []
         y = []
         z = []
-        for x_idx in range(4):
-            for y_idx in range(4):
-                for z_idx in range(4):
-                    x.append(x_idx % 2 * self.scale[0])
-                    y.append(y_idx % 2 * self.scale[1])
-                    z.append(z_idx % 2 * self.scale[2])
+
+        # Base 0,0
+        px = 0
+        py = 0
+        pz = 0
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Col 0
+        pz += self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        pz -= self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Base x+
+        px += self.scale[0]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # col 1
+        pz += self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        pz -= self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Base y+
+        py += self.scale[1]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Col 2
+        pz += self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        pz -= self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Base x-
+        px -= self.scale[0]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        #Col 3
+        pz += self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        pz -= self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Base y-
+        py -= self.scale[1]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Go up
+        pz += self.scale[2]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Ceiling
+        px += self.scale[0]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Base y+
+        py += self.scale[1]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Base x-
+        px -= self.scale[0]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
+
+        # Base y-
+        py -= self.scale[1]
+        x.append(px)
+        y.append(py)
+        z.append(pz)
         return ax.plot3D(x, y, z, c='black')
 
 
