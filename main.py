74b3e3275af257d8bac4be0aa5fa30e531ed4571
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-22 14:26:17 -0500

    added fire fighter class

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from building import Building
+from fire_fighter import Firefighter
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 
 scale = [10, 10, 10]
 
 building = Building(scale)
+fireman_1 = Firefighter(bounds=scale)
 
+print(fireman_1.name)
+fireman_1.random_walk()
+print(fireman_1.location)
 fig = plt.figure()
 b_fig = building.draw(fig)
 
 plt.show()
+
