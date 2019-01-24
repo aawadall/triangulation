e31051f01aa13e1118f55dd9b5444483e18d4a70
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-24 14:19:13 -0500

    added marker to beacon

--- beacon.py
+++ beacon.py
@@ -4,8 +4,9 @@ import string
 
 
 class Beacon(object):
-    def __init__(self, location, name=None, color='black'):
+    def __init__(self, location, name=None, color='black', marker='^'):
         self.location = location
         if name is None:
             self.name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
         self.color = color
+        self.marker = '^'
