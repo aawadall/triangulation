59abc2e1cf93bdf4dd7624b8fd657f8bd4dc84d5
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-24 14:11:17 -0500

    fixed superimposing plots

--- plotter.py
+++ plotter.py
@@ -3,30 +3,41 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
+class Line(object):
+    def __init__(self, x, y, z, color):
+        self.x=x
+        self.y=y
+        self.z=z
+        self.color=color
+
+
 class Plotter(object):
     def __init__(self):
         self.fig = plt.figure()
-        self.ax = []
+        self.lines = []
 
     def draw_line(self, x, y, z, color=None):
-        self.ax.append(Axes3D(self.fig))
-        if color is None:
-            self.ax[-1].plot3D(x, y, z)
-        else:
-            self.ax[-1].plot3D(x, y, z, c=color)
+        line = Line(x, y, z, color)
+        self.lines.append(line)
+        ax = Axes3D(self.fig)
+        for l in self.lines:
+            if l.color is None:
+                ax.plot3D(l.x, l.y, l.z)
+            else:
+                ax.plot3D(l.x, l.y, l.z, c=l.color)
 
     def drw_scatter(self, x, y, z, color=None, marker=None):
-        self.ax.append(Axes3D(self.fig))
+        ax = Axes3D(self.fig)
         if color is None:
             if marker is None:
-                self.ax[-1].scatter(x, y, z)
+                ax.scatter(x, y, z)
             else:
-                self.ax[-1].scatter(x, y, z, marker=marker)
+                ax.scatter(x, y, z, marker=marker)
         else:
             if marker is None:
-                self.ax[-1].scatter(x, y, z, c=color)
+                ax.scatter(x, y, z, c=color)
             else:
-                self.ax[-1].scatter(x, y, z, marker=marker, c=color)
+                ax.scatter(x, y, z, marker=marker, c=color)
 
     def show(self):
         plt.show(self.fig)
