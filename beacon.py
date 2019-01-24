b41532b3f6c846e13c61d0e8f4efa019848a5424
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-24 14:23:45 -0500

    beacons can be plotted

--- beacon.py
+++ beacon.py
@@ -4,9 +4,13 @@ import string
 
 
 class Beacon(object):
-    def __init__(self, location, name=None, color='black', marker='^'):
+    def __init__(self, location, color='black', marker='^', name=None):
         self.location = location
         if name is None:
             self.name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
         self.color = color
-        self.marker = '^'
+        self.marker = marker
+
+    def get_location(self):
+        return self.location[0], self.location[1], self.location[2]
+
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from beacon import Beacon
 from plotter import Plotter
 
 
-scale = [10, 10, 10]
+scale = [100, 100, 10]
 
 building = Building(scale)
 fireman_1 = Firefighter(bounds=scale)
-beacon_1 = Beacon([0, 0, 0])
+beacon_1 = Beacon([-50, -50, 0], 'green')
 
 print("Fireman: ", fireman_1.name)
 fireman_1.random_walk()
@@ -26,4 +26,6 @@ f_x, f_y, f_z = fireman_1.draw()
 plotter.draw_line(f_x, f_y, f_z, color='green')
 plotter.draw_line(b_x, b_y, b_z, color='black')
 
+x, y, z = beacon_1.get_location()
+plotter.drw_scatter(x, y, z, beacon_1.color, beacon_1.marker)
 plotter.show()
