e385bb512f2602b7d663d54a68c69540468b5d54
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-24 14:18:43 -0500

    Added color to beacons

--- beacon.py
+++ beacon.py
@@ -4,7 +4,8 @@ import string
 
 
 class Beacon(object):
-    def __init__(self, location, name=None):
+    def __init__(self, location, name=None, color='black'):
         self.location = location
         if name is None:
-            self.name =  ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+            self.name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+        self.color = color
