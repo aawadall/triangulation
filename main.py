e6c2142d32a3be3f477a3ac6e5f495fa08c45d31
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-29 13:18:33 -0500

    we have a working visualizer

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ from plotter import Plotter
 scale = [100, 100, 10]
 
 building = Building(scale)
-fireman_1 = Firefighter(bounds=scale)
+firemen = []
+
+for f in range(10):
+    firemen.append(Firefighter(bounds=scale, max_v=f/10))
 beacons = [Beacon([-50, -50, 0], 'green'),
            Beacon([scale[0]/2, -50, 0], 'red'),
            Beacon([scale[0] + 50, -50, 0], 'green')
            ]
 
-print("Fireman: ", fireman_1.name)
-fireman_1.random_walk()
-print("Location after random walk", fireman_1.location)
+for _ in range(1000):
+    for fireman_1 in firemen:
+        fireman_1.random_walk()
 
-for _ in range(100):
-    fireman_1.random_walk()
 
 plotter = Plotter()
 
 plotter.draw_line(building.draw())
-plotter.draw_line(fireman_1.draw())
+
+for fireman_1 in firemen:
+    plotter.draw_line(fireman_1.draw())
 
 for beacon_1 in beacons:
     plotter.drw_scatter(beacon_1.draw_beacon())
