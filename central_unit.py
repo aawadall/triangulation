9d220128f761968420fe2b22d59a2fb566af4f02
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-29 15:05:59 -0500

    find beacon, find fireman

--- central_unit.py
+++ central_unit.py
@@ -14,7 +14,18 @@ class CentralUnit(object):
     def register_beacon(self, beacon):
         self.beacons.append(beacon)
 
+    def find_beacon(self, beaon_index):
+        """Given a beacon ID, locate that beacon from known beacons"""
+        pass
+
     def find_fireman(self, fireman_index):
+        """Given known beacon locations, find fireman using its distances from identified beacons"""
+        observed_distances = []
+
+        # Collect distances vector
+        for beacon in self.beacons:
+            observed_distances.append(beacon.get_distance(self.firemen[fireman_index]))
+
         #TODO from fireman index, get its location using its beacons
         pass
 
