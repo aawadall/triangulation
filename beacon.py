09e4c52abd5c0e2cbf2e710f95afc0af315ed4a5
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-29 13:25:45 -0500

    beacons should now measure distance

--- beacon.py
+++ beacon.py
@@ -2,6 +2,7 @@
 import random
 import string
 from plotter import Scatters
+import numpy as np
 
 
 class Beacon(object):
@@ -21,4 +22,11 @@ class Beacon(object):
                         [self.location[2]],
                         self.color,
                         self.marker)
+
+    def get_distance(self, firefighter):
+        """simulate distance reading of a firefighter with noise as function of actual distance"""
+        linear_noise = 10
+        nonlinear_noise = 0.2
+        actual_distance = np.sqrt(np.sum( np.square([self.location[i] - firefighter.location[i] for i in range(3)])))
+        return actual_distance * (1 + random.randrange(-1,1) * nonlinear_noise) + random.randrange(-1,1) * linear_noise
 
